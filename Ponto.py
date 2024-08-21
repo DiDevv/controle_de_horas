6077d06031f3cfe53c96eeac9937c1b3db8be9ba
Diogo Dias <diasdiogo429@gmail.com>
2024-08-21 14:56:00 -0300

    Feat: Input dinâmico com o usuário

--- Ponto.py
+++ Ponto.py
@@ -8,16 +8,27 @@ def filtrar_duracao (tempo):
 
 #O formato da hora deve ser "00:00:00" (Hora, Minutos, Segundos)
 #Desconto é caso você tenha que descontar algumas horas do trabalho no dia
-def tempo_restante(hora_atual:str, horas_totais:str, horas_trabalhadas:str, desconto:str ) -> str:
+def tempo_restante(hora_inicial:str, horas_totais:str, horas_trabalhadas:str, desconto:str ) -> str:
 
-    hora_atual_ = filtrar_duracao(hora_atual)
+    hora_inicial_ = filtrar_duracao(hora_inicial)
     horas_totais_ = filtrar_duracao(horas_totais)
     horas_trabalhadas_hoje_ = filtrar_duracao(horas_trabalhadas)
     desconto_ = filtrar_duracao(desconto)
 
     restam = horas_totais_ - horas_trabalhadas_hoje_ - desconto_
-    trabalhar_ate = hora_atual_ + restam
+    trabalhar_ate = hora_inicial_ + restam
 
-    return f"Ainda faltam {restam} horas, de acordo com o horário atual {hora_atual}, você deverá trabalhar até às {trabalhar_ate}"
+    return f"""
+            Carga horária diária: {horas_totais_}
+            Ainda faltam: {restam},
+            Horário de Início: {hora_inicial}
+            Precisa trabalhar até: {trabalhar_ate}, para atingir as {horas_totais_} horas diárias.
+            """
 
-print(tempo_restante("14:50:00", "06:00:00", "01:41:42", "00:30:00"))
+w = input("Que horas pretende iniciar o turno? (Formato de entrada: 00:00:00) ")
+x = input("Qual a carga horária diária total? (Formato de entrada: 00:00:00) ")
+y = input("Quantas horas já trabalhou hoje? (Formato de entrada: 00:00:00) ")
+z = input("Deseja descontar alguma quantidade de horas? (Formato de entrada: 00:00:00) ")
+
+
+print(tempo_restante(w, x, y,z))

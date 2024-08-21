7340579fb52d31b03c28c7f94015371826af5834
Diogo Dias <diasdiogo429@gmail.com>
2024-08-21 14:45:35 -0300

    Feat: Agora dá para estimar até que horas devo trabalhar

--- Ponto.py
+++ Ponto.py
@@ -8,14 +8,16 @@ def filtrar_duracao (tempo):
 
 #O formato da hora deve ser "00:00:00" (Hora, Minutos, Segundos)
 #Desconto é caso você tenha que descontar algumas horas do trabalho no dia
-def tempo_restante(horas_totais:str, horas_trabalhadas:str, desconto:str, ) -> str:
+def tempo_restante(hora_atual:str, horas_totais:str, horas_trabalhadas:str, desconto:str ) -> str:
 
+    hora_atual_ = filtrar_duracao(hora_atual)
     horas_totais_ = filtrar_duracao(horas_totais)
     horas_trabalhadas_hoje_ = filtrar_duracao(horas_trabalhadas)
     desconto_ = filtrar_duracao(desconto)
 
     restam = horas_totais_ - horas_trabalhadas_hoje_ - desconto_
+    trabalhar_ate = hora_atual_ + restam
 
-    return f"Restam {restam} horas, bom trabalho ^^"
+    return f"Ainda faltam {restam} horas, de acordo com o horário atual {hora_atual}, você deverá trabalhar até às {trabalhar_ate}"
 
-print(tempo_restante("06:00:00", "01:41:42", "00:30:00"))
+print(tempo_restante("14:50:00", "06:00:00", "01:41:42", "00:30:00"))
